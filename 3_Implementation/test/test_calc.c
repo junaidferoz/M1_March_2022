60f1d6e1ce31d929e26b603795bb9c90a182795b
junaidferoz <60928280+junaidferoz@users.noreply.github.com>
2022-04-03 10:53:24 +0530

    changes done to testing part

--- 3_Implementation/test/test_calc.c
+++ 3_Implementation/test/test_calc.c
@@ -17,7 +17,6 @@ void test_squareroot(void);
 void test_comp_add(void);
 void test_comp_subtract(void);
 void test_comp_multiply(void);
-void test_comp_divide(void);
 void test_comp_absolute(void);
 void test_comp_angle(void);
 
@@ -40,6 +39,17 @@ int main(){
     RUN_TEST(test_modulus);
     RUN_TEST(test_factorial);
     RUN_TEST(test_squareroot);
+
+    RUN_TEST(test_comp_add);
+    RUN_TEST(test_comp_subtract);
+    RUN_TEST(test_comp_multiply);
+    RUN_TEST(test_comp_absolute);
+    RUN_TEST(test_comp_angle);
+    
+    RUN_TEST(test_add_vector);
+    RUN_TEST(test_subtract_vector);
+    RUN_TEST(test_multiplydot_vector);
+    RUN_TEST(test_magnitude_vector);
 
     return UNITY_END();
 }
@@ -78,6 +88,68 @@ void test_factorial(void){
     TEST_ASSERT_EQUAL(5098,factorial(7)); //fail case
 }
 
-void test_complex_add(void){
-    
+void test_comp_absolute(void){
+    complex comp = {4,3};
+    TEST_ASSERT_EQUAL(5,absolute(comp));
+    TEST_ASSERT_EQUAL(7,absolute(comp)); //fail case
 }
+void test_comp_add(void){
+    complex num1 = {4,5};
+    complex num2 = {6,7};
+    complex sum = {10,12};
+    TEST_ASSERT_EQUAL(10,addition(num1,num2).real);
+    TEST_ASSERT_EQUAL(12,addition(num1,num2).imaginary);
+}
+void test_comp_subtract(void){
+    complex num1 = {4,5};
+    complex num2 = {6,7};
+    complex difference = {-2,-2};
+    TEST_ASSERT_EQUAL(-2,subtraction(num1,num2).real);
+    TEST_ASSERT_EQUAL(-2,subtraction(num1,num2).imaginary);
+}
+void test_comp_multiply(void){
+    complex num1 = {11,6};
+    complex num2 = {6,10};
+    complex product = multiplication(num1,num2);
+    TEST_ASSERT_EQUAL(product.real,multiplication(num1,num2).real);
+    TEST_ASSERT_EQUAL(-7,multiplication(num1,num2).imaginary); //fail case
+    TEST_ASSERT_EQUAL(product.imaginary,multiplication(num1,num2).imaginary);
+}
+void test_comp_angle(void){
+    complex num = {1,1};
+    double ang = angle(num);
+    TEST_ASSERT_EQUAL(ang,angle(num));
+    TEST_ASSERT_EQUAL(7.00,angle(num)); //fail case
+}
+void test_add_vector(void){
+    vector vect1 = {3,4,5};
+    vector vect2 = {-1,2,-4};
+    vector ans = sum(vect1,vect2);
+    TEST_ASSERT_EQUAL(ans.x,vect1.x+vect2.x);
+    TEST_ASSERT_EQUAL(ans.y,vect1.y+vect2.y);
+    TEST_ASSERT_EQUAL(ans.z,vect1.z+vect2.z);
+    TEST_ASSERT_EQUAL(5,sum(vect1,vect2).x); //fail case
+}
+void test_subtract_vector(void){
+    vector vect1 = {3,4,5};
+    vector vect2 = {-1,2,-4};
+    vector diff = difference(vect1,vect2);
+    TEST_ASSERT_EQUAL(diff.x,vect1.x-vect2.x);
+    TEST_ASSERT_EQUAL(diff.y,vect1.y-vect2.y);
+    TEST_ASSERT_EQUAL(diff.z,vect1.z-vect2.z);
+    TEST_ASSERT_EQUAL(5,difference(vect1,vect2).x); //fail case
+}
+void test_multiplydot_vector(void){
+    vector vect = {3,2,1};
+    vector another_vect = {4,5,-2};
+    int ans = dot(vect,another_vect);
+    TEST_ASSERT_EQUAL(20,ans);
+    TEST_ASSERT_EQUAL(10,ans); //fail case
+}
+void test_magnitude_vector(void){
+    vector vect = {2,2,1};
+    double ans = magnitude(vect);
+    TEST_ASSERT_EQUAL(3.00,ans);
+    TEST_ASSERT_EQUAL(2.00,ans); //fail case
+}
+
